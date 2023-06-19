b45d1c67280ac5836f1acaabacb6ba191bfaa258
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-06-19 12:01:20 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-import models
+from models import storage_type
 from models.city import City
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    if models.is_type == "db":
-        __tablename__ = 'states'
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
-    if models.is_type != 'db':
         @property
         def cities(self):
-            cities_list = []
-            all_cities = models.storage.all(City).values()
-            for city in all_cities:
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
                 if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+                    related_cities.append(city)
+            return related_cities
